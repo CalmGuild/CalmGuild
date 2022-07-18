fffe621378b671c118fafadc22c5398bcd53dd1f
Miqhtiedev <miqhtiedev@gmail.com>
2022-07-18 18:19:22 -0400

    feat: user mentions accepted in user input parser

--- apps/calmbot/src/utils/getUserFromInput.ts
+++ apps/calmbot/src/utils/getUserFromInput.ts
@@ -1,21 +1,25 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Client, User } from "discord.js";
+import { Client, MessageMentions, User } from "discord.js";
 import getUUIDFromName from "./apis/mojang";
 import isProperId from "./regex/isProperId";
 import isProperMinecraftUsername from "./regex/isProperMinecraftUsername";
 import database from "@calmguild/database";
 
-type InputTypes = "DISCORD_ID" | "MINECRAFT_USERNAME" | "MINECRAFT_UUID";
+type InputTypes = "DISCORD_ID" | "DISCORD_MENTION" | "MINECRAFT_USERNAME" | "MINECRAFT_UUID";
 
-export default (client: Client, input: string, allowedInputTypes: InputTypes[] = ["DISCORD_ID", "MINECRAFT_USERNAME", "MINECRAFT_UUID"]): Promise<User | undefined> => {
+export default (client: Client, input: string, allowedInputTypes: InputTypes[] = ["DISCORD_ID", "MINECRAFT_USERNAME", "MINECRAFT_UUID", "DISCORD_MENTION"]): Promise<User | undefined> => {
   return new Promise(async (resolve) => {
-    // check if input is a discord id
     if (allowedInputTypes.includes("DISCORD_ID") && isProperId(input)) {
       const user = await client.users.fetch(input).catch(() => {});
       if (user) return resolve(user);
     }
 
-    // check if input is a valid minecraft name
+    if (allowedInputTypes.includes("DISCORD_MENTION") && MessageMentions.USERS_PATTERN.test(input)) {
+      const id = input.slice(2, -1);
+      const user = await client.users.fetch(id).catch(() => {});
+      if (user) return resolve(user);
+    }
+
     if (allowedInputTypes.includes("MINECRAFT_USERNAME") && isProperMinecraftUsername(input)) {
       const uuid = await getUUIDFromName(input).catch(() => {});
       if (uuid) {
