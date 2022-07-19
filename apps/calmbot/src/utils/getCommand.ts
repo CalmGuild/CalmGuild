800ea0d1afa0b47525c475a060a0608a97ffe3d3
Miqhtiedev <miqhtiedev@gmail.com>
2022-07-19 15:55:28 -0400

    feat: default subcommand alias working in command handler

--- apps/calmbot/src/utils/getCommand.ts
+++ apps/calmbot/src/utils/getCommand.ts
@@ -2,5 +2,16 @@ import { Collection } from "discord.js";
 import { Command } from "../client/command";
 
 export const getCommand = (commands: Collection<string, Command>, name: string): Command | undefined => {
-  return commands.get(name.toLowerCase()) ?? commands.find((command) => command.aliases !== undefined && command.aliases.includes(name.toLowerCase()));
+  let command: Command | undefined = undefined;
+
+  // command matches name
+  command = commands.get(name.toLowerCase());
+  if (command) return command;
+
+  // command alias matches name
+  command = commands.find((command) => command.aliases !== undefined && command.aliases.includes(name.toLowerCase()));
+  if (command) return command;
+
+  // default subcommand with alias matches name
+  return commands.find((c) => c.subcommands?.find((subcommand) => subcommand.defaultSubcommand !== undefined && subcommand.aliases !== undefined && subcommand.aliases.includes(name.toLowerCase())) !== undefined);
 };
